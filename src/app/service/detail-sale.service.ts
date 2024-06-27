2d70e48034e43f0a2901b274f1beb1baf98ae4a2
meiuwu2001 <samuel.reyna434@gmail.com>
2024-06-27 17:34:12 -0600

    paeldiego

---
+++ src/app/service/detail-sale.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../model/product';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetailSaleService {
+  private productList: Product[] = []
+  private products: BehaviorSubject<Product[]>
+  constructor() {
+    this.products = new BehaviorSubject(this.productList)
+  }
+
+  getProductSale() {
+    return this.products.asObservable()
+
+  }
+
+  addProductSale(product: Product) {
+    product.amount = 1
+    this.productList.push(product)
+    this.products.next(this.productList)
+  }
+
+  deleteProductSale(id: String) {
+    let index = this.productList.findIndex(item => item._id === id)
+    this.productList.splice(index, 1)
+    this.products.next(this.productList)
+  }
+}
